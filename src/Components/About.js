eddebf03974974fa122c186ce61b238a21ca3db3
ErnestoVM <arkangel_121@hotmail.com>
2025-01-08 21:20:35 -0600

    Update package.json and enhance About and Footer components with improved formatting and contact details

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,50 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= process.env.PUBLIC_URL + '/images/' + this.props.data.image;
+      var profilepic =
+        process.env.PUBLIC_URL + "/images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="profile picture" />
-         </div>
-         <div className="nine columns main-col">
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="profile-pic"
+              src={profilepic}
+              alt="profile picture"
+            />
+          </div>
+          <div className="nine columns main-col">
             <h2>Acerca de mi</h2>
 
             <p>{bio}</p>
             <div className="row">
-               <div className="columns contact-details">
-                  <h2>Detalles de Contacto</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{city}, {state}, {zip}</span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Descargar Curriculum</a>
-                  </p>
-               </div>
+              <div className="columns contact-details">
+                <h2>Detalles de Contacto</h2>
+                <p className="address">
+                  <i className="fa fa-user"></i> <span>{name}</span>
+                  <br />
+                  <span>
+                    <i className="fa fa-map-marker"></i> {city}, {state}
+                  </span>
+                  <br />
+                  <i className="fa fa-phone"></i> <span>{phone}</span>
+                  <br />
+                  <i className="fa fa-envelope"></i> <span>{email}</span>
+                </p>
+              </div>
+              <div className="columns download">
+                <p>
+                  <a href={resumeDownload} target="_blank" className="button">
+                    <i className="fa fa-download"></i>Descargar Curriculum
+                  </a>
+                </p>
+              </div>
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
